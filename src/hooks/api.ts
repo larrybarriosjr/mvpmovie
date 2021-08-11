fe56e8f129a34899c3e034a5e6a9b080553843d3
Larry Barrios Jr <github@larrybarriosjr.com>
2021-08-11 19:53:11 +0800

    Remove redundant api folder

--- src/hooks/api.ts
+++ src/hooks/api.ts
@@ -1,13 +1,15 @@
-import { getPopular, getTrending } from "api/movies"
 import { AxiosResponse } from "axios"
-import { ApiRouteKey } from "constants/enum"
+import { ApiRouteKey, ApiRoutePath } from "constants/enum"
 import { useQuery } from "react-query"
+import { Axios } from "services/web"
 import { MovieType, TrendingMovieType } from "types/tmdb"
 
 export const useGetPopular = () => {
+  const getPopular = () => Axios.get(ApiRoutePath.POPULAR)
   return useQuery<AxiosResponse<MovieType[]>>(ApiRouteKey.POPULAR, getPopular)
 }
 
 export const useGetTrending = () => {
+  const getTrending = () => Axios.get(ApiRoutePath.TRENDING)
   return useQuery<AxiosResponse<TrendingMovieType[]>>(ApiRouteKey.TRENDING, getTrending)
 }
