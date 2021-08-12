23ff7a22288ed45e52e23ff61a92c0f9e0d2615b
Larry Barrios Jr <github@larrybarriosjr.com>
2021-08-12 23:25:58 +0800

    Componentize pagination

--- src/pages/ListPage.tsx
+++ src/pages/ListPage.tsx
@@ -1,10 +1,8 @@
+import Pagination from "components/Pagination"
 import Section from "components/Section"
 import { PAGE_SIZE } from "constants/default"
 import { RoutePath } from "constants/enum"
-import Pagination from "rc-pagination"
-import EnUsLocale from "rc-pagination/lib/locale/en_US"
 import { Fragment } from "react"
-import { RiArrowLeftSFill, RiArrowRightSFill, RiMoreFill } from "react-icons/ri"
 import { useLocation } from "react-router-dom"
 import { MovieType } from "types/movies"
 
@@ -16,7 +14,7 @@ type ListPageProps = {
   toggleFavorites: (movie: MovieType) => void
   currentPage: number
   totalItems: number
-  onPageChange: (page: number, pageSize: number) => void
+  onPageChange: (page: number) => void
 }
 
 const ListPage = ({
@@ -42,19 +40,7 @@ const ListPage = ({
         favorites={favorites}
         toggleFavorites={toggleFavorites}
       />
-      <Pagination
-        className="flex items-center justify-center my-4 text-lg gap-x-4"
-        current={currentPage}
-        pageSize={PAGE_SIZE}
-        total={Math.min(2_000, totalItems)}
-        prevIcon={<RiArrowLeftSFill className="cursor-pointer" size="24" />}
-        nextIcon={<RiArrowRightSFill className="cursor-pointer" size="24" />}
-        jumpNextIcon={<RiMoreFill />}
-        jumpPrevIcon={<RiMoreFill />}
-        locale={EnUsLocale}
-        onChange={onPageChange}
-        hideOnSinglePage
-      />
+      <Pagination currentPage={currentPage} totalItems={totalItems} onPageChange={onPageChange} />
     </Fragment>
   )
 }
