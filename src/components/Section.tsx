865c8d06a2caf12f14570daf1b2576baf8122aeb
Larry Barrios Jr <github@larrybarriosjr.com>
2021-08-14 15:31:36 +0800

    Toggle favorites when clicking on the movie poster

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -55,14 +55,14 @@ const Section = ({
             .filter((movie: MovieType) => movie.ids.tmdb === movieUrl(movie)?.id)
             .map((movie: MovieType) => (
               <li key={movie.ids.trakt} className="flex flex-col gap-2">
-                <div className="relative p-1 overflow-hidden cursor-pointer hover:ring-4 hover:ring-primary">
-                  <Link to={`${RoutePath.DETAILS}/${movie.ids.trakt}`}>
-                    <img
-                      src={IMAGE_URL + movieUrl(movie).url}
-                      alt={movie.title}
-                      className="object-cover border border-white border-solid w-60 h-96"
-                    />
-                  </Link>
+                <div className="relative p-1 overflow-hidden hover:ring-4 hover:ring-primary h-96">
+                  <input
+                    type="image"
+                    src={IMAGE_URL + movieUrl(movie).url}
+                    alt={movie.title}
+                    className="object-cover h-full border border-white border-solid cursor-pointer w-60"
+                    onClick={() => toggleFavorites(movie)}
+                  />
                   <button
                     type="button"
                     className="absolute rounded-full right-2 bottom-2 bg-gray"
@@ -87,7 +87,7 @@ const Section = ({
       ) : url === RoutePath.FAVORITES ? (
         <div className="mt-8 mb-16 text-lg text-center">
           <p>You haven&apos;t added your favorite movies yet.</p>
-          <p>Search for a movie and click on one of the stars (&#9734;) to add one.</p>
+          <p>Click on one of the movie posters to add one.</p>
         </div>
       ) : null}
       {currentPage && totalItems && onPageChange ? (
